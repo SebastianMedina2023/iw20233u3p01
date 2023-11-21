9875e285fa9ead83a522c081120d627a559e06da
Sebastián Jazael Medina Rodríguez <sejamedinaro@ittepic.edu.mx>
2023-11-21 07:54:06 -0700

    Historial

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Cart } from '../models/product.model';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +11,18 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  public cart: Observable<Cart[]>;
 
+  constructor(private cartService:CartService) {
+    this.cart =this.cartService.getCartH()
+    this.getHistory();
+  }
+
+  public getHistory(){
+    return this.cart.forEach((result)=>{
+      console.log(result.forEach(product=>{
+        console.log(product.itemCount,product.total, product.items);
+      }));
+    });
+  }
 }
